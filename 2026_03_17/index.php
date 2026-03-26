b8840a3236de2aef64124f18d0c11f503c3fb6ae
VickyHornet <victormoreno10@aragon.unam.mx>
2026-03-26 15:29:56 -0600

    Add files via upload

---
+++ 2026_03_17/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+
+<form action="logica.php" method="get">
+    <label for="">Nombre:</label>
+    <input type="text" maxlength="35" name="nombre">
+    <!-- <hr>
+    <label for="">1er Apellido</label>
+    <input type="text"  required name="1erApellido">
+    <hr>
+    <label for="">2do Apellido</label>
+    <input type="text" name="2doApellido">
+    <hr>
+    <label for="">Edad</label>
+    <input type="number" name="edad">
+    <hr>
+    <label for="">Correo</label>
+    <input type="email" name="correo">
+    <hr>
+    <label for="">Contraseña</label>
+    <input type="password" name="password"> -->
+
+
+    <button type="submit">Enviar</button>
+
+</form>
+</body>
+</html>
---
+++ 2026_03_17/logica.php
@@ -0,0 +1,23 @@
+<?php
+$nombreUsuario = $_GET['nombre'];
+echo $nombreUsuario;
+
+// $primerApellido = $_GET['1erApellido'];
+// echo $primerApellido;
+
+// $segundoApellido = $_GET['2doApellido'];
+// echo $segundoApellido;
+
+// $edad = $_GET['edad'];
+// echo $edad;
+
+// $correo = $_GET['correo'];
+// echo $correo;
+
+// $password = $_GET['password'];
+// echo $password;
+// echo "<br>";
+// echo "<h1>".$nombreUsuario."</h1><br>"."<br> 1er Apellido".$primerApellido;
+
+// $suma = $edad +10;
+// echo "<br>".$suma;
